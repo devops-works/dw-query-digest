d0b77be6e8202a54a06876ab36e9610b2390214e
Michel Blanc <mb@devops.works>
2019-01-06 15:19:33 +0100

    Removed empty line

--- main_test.go
+++ main_test.go
@@ -30,7 +30,6 @@ func TestFingerprint(t *testing.T) {
 		{"SELECT foo, bar FROM table WHERE foo=5 AND bar=`baz`", "select foo, bar from table where foo = ? and bar = ?"},
 		{`SELECT foo, bar FROM table WHERE foo=5 AND bar='baz'`, `select foo, bar from table where foo = ? and bar = ?`},
 		{"UPDATE `something` set `a`=4, `b`=false, `c`='1' WHERE `a`=300662 AND `c`=`2`", "update `something` set `a` = ?, `b` = ?, `c` = ? where `a` = ? and `c` = ?"},
-
 		// 6·   Collapse all whitespace into a single space.
 		{`SELECT  *  FROM  table  `, `select * from table`},
 		// 7·   Lowercase the entire query.
