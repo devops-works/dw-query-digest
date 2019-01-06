f5e5064e0af4df4f4f61458d6f43ddb1621390e9
Michel Blanc <mb@devops.works>
2019-01-06 15:19:10 +0100

    Adds test
    Added UPDATE test

--- main_test.go
+++ main_test.go
@@ -29,6 +29,8 @@ func TestFingerprint(t *testing.T) {
 		// 5·   Replace all literals, such as quoted strings
 		{"SELECT foo, bar FROM table WHERE foo=5 AND bar=`baz`", "select foo, bar from table where foo = ? and bar = ?"},
 		{`SELECT foo, bar FROM table WHERE foo=5 AND bar='baz'`, `select foo, bar from table where foo = ? and bar = ?`},
+		{"UPDATE `something` set `a`=4, `b`=false, `c`='1' WHERE `a`=300662 AND `c`=`2`", "update `something` set `a` = ?, `b` = ?, `c` = ? where `a` = ? and `c` = ?"},
+
 		// 6·   Collapse all whitespace into a single space.
 		{`SELECT  *  FROM  table  `, `select * from table`},
 		// 7·   Lowercase the entire query.
